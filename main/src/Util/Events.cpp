878c82c1a628f07d2ff4647cfaf4c4e86e752b36
Filip Budiša <filip@circuitmess.com>
2023-05-29 15:10:39 +0200

    Add Queue and EventSystem

---
+++ main/src/Util/Events.cpp
@@ -0,0 +1,41 @@
+#include "Events.h"
+#include <cstring>
+
+std::unordered_map<Facility, std::unordered_set<EventQueue*>> Events::queues;
+
+void Events::listen(Facility facility, EventQueue* queue){
+	queues[facility].insert(queue);
+}
+
+void Events::post(Facility facility, const void* data, size_t size){
+	auto pair = queues.find(facility);
+	if(pair == queues.end()) return;
+
+	for(auto& queue: pair->second){
+		void* qData = malloc(size);
+		memcpy(qData, data, size);
+		queue->post(facility, qData);
+	}
+}
+
+
+EventQueue::EventQueue(size_t count){
+	queue = xQueueCreate(count, sizeof(Event));
+}
+
+EventQueue::~EventQueue(){
+	vQueueDelete(queue);
+}
+
+bool EventQueue::get(Event& event, TickType_t timeout){
+	return xQueueReceive(queue, &event, timeout) == pdTRUE;
+}
+
+bool EventQueue::post(Facility facility, void* data){
+	Event event = {
+			.facility = facility,
+			.data = data
+	};
+
+	return xQueueSend(queue, &event, 0) == pdTRUE;
+}
---
+++ main/src/Util/Events.h
@@ -0,0 +1,55 @@
+#ifndef CLOCKSTAR_FIRMWARE_EVENTS_H
+#define CLOCKSTAR_FIRMWARE_EVENTS_H
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/portmacro.h>
+#include <freertos/queue.h>
+#include <unordered_map>
+#include <unordered_set>
+
+enum class Facility { Events };
+
+struct Event {
+	Facility facility;
+	void* data;
+};
+
+class EventQueue;
+class Events {
+public:
+	static void listen(Facility facility, EventQueue* queue);
+
+	static void post(Facility facility, const void* data, size_t size);
+
+	template <typename T>
+	static void post(Facility facility, const T& data){
+		post(facility, &data, sizeof(T));
+	}
+
+	template <typename T>
+	static void post(Facility facility, const T* data){
+		post(facility, data, sizeof(T));
+	}
+
+private:
+	static std::unordered_map<Facility, std::unordered_set<EventQueue*>> queues;
+
+};
+
+class EventQueue {
+public:
+	EventQueue(size_t count);
+	virtual ~EventQueue();
+
+	bool get(Event& item, TickType_t timeout);
+
+private:
+	QueueHandle_t queue;
+
+	bool post(Facility facility, void* data);
+	friend Events;
+
+};
+
+
+#endif //CLOCKSTAR_FIRMWARE_EVENTS_H
