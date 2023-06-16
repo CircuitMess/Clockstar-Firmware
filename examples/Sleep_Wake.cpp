33d89cc48270492d32ba3103bb67a85535c47af8
Filip Budiša <filip@circuitmess.com>
2023-06-16 22:53:32 +0200

    Add IMU to Sleep_Wake test

--- examples/Sleep_Wake.cpp
+++ examples/Sleep_Wake.cpp
@@ -5,6 +5,8 @@
 #include <esp_log.h>
 #include <esp_sleep.h>
 #include <memory>
+#include <Periph/I2C.h>
+#include <Devices/IMU.h>
 #include <Devices/Input.h>
 #include "Util/Events.h"
 #include "Pins.hpp"
@@ -18,6 +20,7 @@
 
 void sleep(){
 	printf("Going to sleep...\n");
+	printf("INT2 is %d\n", gpio_get_level((gpio_num_t) IMU_INT2));
 	vTaskDelay(100);
 	esp_light_sleep_start();
 	printf("Awake!\n");
@@ -26,18 +29,26 @@ void sleep(){
 [[noreturn]] void btnProcess(void* arg){
 	EventQueue events(12);
 	Events::listen(Facility::Input, &events);
+	Events::listen(Facility::Motion, &events);
 
 	Event evt;
 	for(;;){
 		if(events.get(evt, portMAX_DELAY) == false) continue;
-		std::unique_ptr<Input::Data> data((Input::Data*) evt.data);
+		if(evt.facility == Facility::Motion){
+			std::unique_ptr<IMU::Event> data((IMU::Event*) evt.data);
+			if(data->action == IMU::Event::WristTilt){
+				printf("Wrist tilt\n");
+			}
 
-		if(data->action != Input::Data::Press) continue;
+			sleep();
+		}else if(evt.facility == Facility::Input){
+			std::unique_ptr<Input::Data> data((Input::Data*) evt.data);
+			if(data->action != Input::Data::Press) continue;
 
-		printf("Pressed %s\n", Input::PinLabels.at(data->btn));
+			printf("Pressed %s\n", Input::PinLabels.at(data->btn));
 
-		vTaskDelay(1000);
-		sleep();
+			sleep();
+		}
 	}
 }
 
@@ -85,11 +96,19 @@ void init(){
 	TaskHandle_t procTask;
 	xTaskCreatePinnedToCore(btnProcess, "Process", 12 * 1024, nullptr, 5, &procTask, 0);
 
+	auto i2c = new I2C(I2C_NUM_0, (gpio_num_t) I2C_SDA, (gpio_num_t) I2C_SCL);
+	auto imu = new IMU(*i2c);
+	imu->init();
+	imu->enableMotionDetection(true);
+	imu->setWristPosition(IMU::WatchPosition::FaceUp);
+	imu->setTiltDirection(IMU::TiltDirection::Lifted);
+
 	esp_sleep_enable_gpio_wakeup();
 	gpio_wakeup_enable((gpio_num_t) BTN_UP, GPIO_INTR_HIGH_LEVEL);
 	gpio_wakeup_enable((gpio_num_t) BTN_DOWN, GPIO_INTR_HIGH_LEVEL);
 	gpio_wakeup_enable((gpio_num_t) BTN_SELECT, GPIO_INTR_HIGH_LEVEL);
 	gpio_wakeup_enable((gpio_num_t) BTN_ALT, GPIO_INTR_HIGH_LEVEL);
+	gpio_wakeup_enable((gpio_num_t) IMU_INT2, GPIO_INTR_HIGH_LEVEL);
 
 	auto display = new Display();
 	auto lvgl = new LVGL(*display);
