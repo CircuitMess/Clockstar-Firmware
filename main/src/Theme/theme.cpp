3552a4ea8c33b21fab22f46c315ee489fabb5ebf
Filip Budiša <filip@circuitmess.com>
2023-06-30 00:21:19 +0200

    Add default black bg to screens

--- main/src/Theme/theme.cpp
+++ main/src/Theme/theme.cpp
@@ -4,16 +4,23 @@
 static lv_theme_t theme;
 
 static struct {
+	lv_style_t scr;
 	lv_style_t label;
 } styles;
 
 static void theme_apply(lv_theme_t* th, lv_obj_t* obj){
-	if(lv_obj_check_type(obj, &lv_label_class)) {
+	if(lv_obj_get_parent(obj) == nullptr){
+		lv_obj_add_style(obj, &styles.scr, 0);
+	}else if(lv_obj_check_type(obj, &lv_label_class)) {
 		lv_obj_add_style(obj, &styles.label, 0);
 	}
 }
 
 lv_theme_t* theme_init(lv_disp_t* disp){
+	lv_style_init(&styles.scr);
+	lv_style_set_bg_color(&styles.scr, lv_color_black());
+	lv_style_set_bg_opa(&styles.scr, LV_OPA_COVER);
+
 	lv_style_init(&styles.label);
 	lv_style_set_text_font(&styles.label, &devin);
 	lv_style_set_text_color(&styles.label, lv_color_make(207, 198, 184));
