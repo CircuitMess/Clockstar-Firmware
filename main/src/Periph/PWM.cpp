3652fa4961c7b1ef322545bf3f1f07e17112e5e0
Emil Gajšak <emil.gajsak@gmail.com>
2023-06-30 14:57:34 +0200

    Added frequency-dutyResolution correctness in PWM::setFreq

--- main/src/Periph/PWM.cpp
+++ main/src/Periph/PWM.cpp
@@ -1,4 +1,8 @@
+#include <esp_clk_tree.h>
+#include <esp_log.h>
 #include "PWM.h"
+
+static const char* TAG = "PMW";
 
 PWM::PWM(uint8_t pin, ledc_channel_t channel) : pin(pin), channel(channel){
 	ledc_timer_config(&ledc_timer);
@@ -9,9 +13,20 @@ void PWM::setFreq(uint16_t freq){
 	if(pin == (uint8_t) -1) return;
 
 	attach();
-	ledc_set_freq(speedMode, LEDC_TIMER_0, freq);
-	ledc_set_duty(speedMode, channel, 0x1FF);
-	ledc_update_duty(speedMode, channel);
+	uint64_t divParam;
+	uint32_t precision = (0x1 << DutyResDefault); // 2**depth
+	uint32_t src_clk_freq;
+	esp_clk_tree_src_get_freq_hz((soc_module_clk_t) LEDC_SLOW_CLK_APB, ESP_CLK_TREE_SRC_FREQ_PRECISION_CACHED, &src_clk_freq);
+	divParam = (((uint64_t) src_clk_freq << 8) + ((freq * precision) / 2)) / (freq * precision);
+
+	if(!(divParam > 256 && divParam < (0x3FFFF))){
+		ESP_LOGW(TAG, "couldnt write frequency %d because of clock divisor limitations, divParam: %lld\n", freq, divParam);
+		return;
+	}
+
+	ledc_set_freq(SpeedMode, LEDC_TIMER_0, freq);
+	ledc_set_duty(SpeedMode, channel, FullDuty);
+	ledc_update_duty(SpeedMode, channel);
 }
 
 void PWM::stop(){
@@ -22,16 +37,17 @@ void PWM::stop(){
 void PWM::attach(){
 	ledc_channel_config_t ledc_channel = {
 			.gpio_num       = pin,
-			.speed_mode     = speedMode,
+			.speed_mode     = SpeedMode,
 			.channel        = channel,
 			.intr_type      = LEDC_INTR_DISABLE,
-			.timer_sel      = LEDC_TIMER_0,
+			.timer_sel      = Timer,
 			.duty           = 0,
-			.hpoint         = 0
+			.hpoint         = 0,
+			.flags = { .output_invert = 1 }
 	};
 	ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
 void PWM::detach(){
-	ledc_stop(speedMode, channel, 0);
+	ledc_stop(SpeedMode, channel, 0);
 }
--- main/src/Periph/PWM.h
+++ main/src/Periph/PWM.h
@@ -1,7 +1,7 @@
 #ifndef CIRCUITOS_PIEZO_H
 #define CIRCUITOS_PIEZO_H
 
-#include <driver/timer.h>
+#include <driver/gptimer.h>
 #include <driver/ledc.h>
 
 class PWM {
@@ -16,14 +16,17 @@ private:
 	void detach();
 	uint8_t pin = -1;
 	ledc_channel_t channel = LEDC_CHANNEL_0;
-	static constexpr ledc_mode_t speedMode = LEDC_LOW_SPEED_MODE;
-	static constexpr ledc_timer_config_t ledc_timer = {
-			.speed_mode       = speedMode,
-			.duty_resolution  = LEDC_TIMER_10_BIT,
+	static constexpr ledc_mode_t SpeedMode = LEDC_LOW_SPEED_MODE;
+	static constexpr ledc_timer_t Timer = LEDC_TIMER_0;
+	ledc_timer_config_t ledc_timer = {
+			.speed_mode       = SpeedMode,
+			.duty_resolution  = DutyResDefault,
 			.timer_num        = LEDC_TIMER_0,
-			.freq_hz          = 5000,  // Set output frequency at 5 kHz
+			.freq_hz          = 5000,
 			.clk_cfg          = LEDC_AUTO_CLK
 	};
+	static constexpr ledc_timer_bit_t DutyResDefault = LEDC_TIMER_10_BIT;
+	static constexpr uint32_t FullDuty = (1 << (LEDC_TIMER_10_BIT - 1)) - 1;
 };
 
 #endif //CIRCUITOS_PIEZO_H
