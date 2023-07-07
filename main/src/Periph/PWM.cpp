9bf79b82098a7f454ad8176f627c90ef4e597f83
Emil Gajšak <emil.gajsak@gmail.com>
2023-07-07 11:29:25 +0200

    PWM abstraction fixed for multiple channels

--- main/src/Periph/PWM.cpp
+++ main/src/Periph/PWM.cpp
@@ -5,14 +5,29 @@
 static const char* TAG = "PMW";
 
 PWM::PWM(uint8_t pin, ledc_channel_t channel) : pin(pin), channel(channel){
-	ledc_timer_config(&ledc_timer);
 
+	ledc_timer_config_t ledc_timer = {
+			.speed_mode       = getSpeedMode(channel),
+			.duty_resolution  = DutyResDefault,
+			.timer_num        = getTimer(channel),
+			.freq_hz          = DefaultFreq,
+			.clk_cfg          = LEDC_AUTO_CLK
+	};
+	if(ledc_timer_config(&ledc_timer) != ESP_OK){
+		ESP_LOGE(TAG, "timer config failed!");
+		return;
+	}
+}
+
+PWM::~PWM(){
+	stop();
 }
 
 void PWM::setFreq(uint16_t freq){
 	if(pin == (uint8_t) -1) return;
 
 	attach();
+
 	uint64_t divParam;
 	uint32_t precision = (0x1 << DutyResDefault); // 2**depth
 	uint32_t src_clk_freq;
@@ -24,9 +39,12 @@ void PWM::setFreq(uint16_t freq){
 		return;
 	}
 
-	ledc_set_freq(SpeedMode, LEDC_TIMER_0, freq);
-	ledc_set_duty(SpeedMode, channel, FullDuty);
-	ledc_update_duty(SpeedMode, channel);
+	auto group = getSpeedMode(channel);
+	auto timer = getTimer(channel);
+
+	ledc_set_freq(group, timer, freq);
+	ledc_set_duty(group, channel, FullDuty);
+	ledc_update_duty(group, channel);
 }
 
 void PWM::stop(){
@@ -35,12 +53,16 @@ void PWM::stop(){
 }
 
 void PWM::attach(){
+	if(pin == (uint8_t) -1) return;
+
+	uint8_t group = (channel / 8);
+	uint8_t timer = ((channel / 2) % 4);
 	ledc_channel_config_t ledc_channel = {
 			.gpio_num       = pin,
-			.speed_mode     = SpeedMode,
+			.speed_mode     = static_cast<ledc_mode_t>(group),
 			.channel        = channel,
 			.intr_type      = LEDC_INTR_DISABLE,
-			.timer_sel      = Timer,
+			.timer_sel      = static_cast<ledc_timer_t>(timer),
 			.duty           = 0,
 			.hpoint         = 0,
 			.flags = { .output_invert = 1 }
@@ -49,5 +71,13 @@ void PWM::attach(){
 }
 
 void PWM::detach(){
-	ledc_stop(SpeedMode, channel, 0);
+	ledc_stop(getSpeedMode(channel), channel, 0);
+}
+
+constexpr ledc_mode_t PWM::getSpeedMode(ledc_channel_t channel){
+	return static_cast<ledc_mode_t>((channel / 8));
+}
+
+constexpr ledc_timer_t PWM::getTimer(ledc_channel_t channel){
+	return static_cast<ledc_timer_t>(((channel / 2) % 4));
 }
--- main/src/Periph/PWM.h
+++ main/src/Periph/PWM.h
@@ -4,9 +4,33 @@
 #include <driver/gptimer.h>
 #include <driver/ledc.h>
 
+/**
+ * Duty resolution defaults to 10-bit (usually enough for most uses such as a piezo buzzer or LED dimming).
+ *
+ *
+ * LEDC Chan to Group/Channel/Timer Mapping (used to prevent conflicts between channels/timers/groups)
+** ledc: 0  => Group: 0, Channel: 0, Timer: 0
+** ledc: 1  => Group: 0, Channel: 1, Timer: 0
+** ledc: 2  => Group: 0, Channel: 2, Timer: 1
+** ledc: 3  => Group: 0, Channel: 3, Timer: 1
+** ledc: 4  => Group: 0, Channel: 4, Timer: 2
+** ledc: 5  => Group: 0, Channel: 5, Timer: 2
+** ledc: 6  => Group: 0, Channel: 6, Timer: 3
+** ledc: 7  => Group: 0, Channel: 7, Timer: 3
+** ledc: 8  => Group: 1, Channel: 0, Timer: 0
+** ledc: 9  => Group: 1, Channel: 1, Timer: 0
+** ledc: 10 => Group: 1, Channel: 2, Timer: 1
+** ledc: 11 => Group: 1, Channel: 3, Timer: 1
+** ledc: 12 => Group: 1, Channel: 4, Timer: 2
+** ledc: 13 => Group: 1, Channel: 5, Timer: 2
+** ledc: 14 => Group: 1, Channel: 6, Timer: 3
+** ledc: 15 => Group: 1, Channel: 7, Timer: 3
+ */
+
 class PWM {
 public:
 	PWM(uint8_t pin, ledc_channel_t channel);
+	virtual ~PWM();
 
 	void setFreq(uint16_t freq);
 	void stop();
@@ -16,17 +40,13 @@ private:
 	void detach();
 	uint8_t pin = -1;
 	ledc_channel_t channel = LEDC_CHANNEL_0;
-	static constexpr ledc_mode_t SpeedMode = LEDC_LOW_SPEED_MODE;
-	static constexpr ledc_timer_t Timer = LEDC_TIMER_0;
-	ledc_timer_config_t ledc_timer = {
-			.speed_mode       = SpeedMode,
-			.duty_resolution  = DutyResDefault,
-			.timer_num        = LEDC_TIMER_0,
-			.freq_hz          = 5000,
-			.clk_cfg          = LEDC_AUTO_CLK
-	};
+	static constexpr uint32_t DefaultFreq = 5000;    //placeholder, usually changed before attaching to a channel or pin
+
 	static constexpr ledc_timer_bit_t DutyResDefault = LEDC_TIMER_10_BIT;
-	static constexpr uint32_t FullDuty = (1 << (LEDC_TIMER_10_BIT - 1)) - 1;
+	static constexpr uint32_t FullDuty = (1 << (DutyResDefault - 1)) - 1;
+
+	static constexpr ledc_mode_t getSpeedMode(ledc_channel_t channel);
+	static constexpr ledc_timer_t getTimer(ledc_channel_t channel);
 };
 
 #endif //CIRCUITOS_PIEZO_H
