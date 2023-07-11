3b561f25b82b0517807e2fad26c9a27a6c32c28e
Emil Gajšak <emil.gajsak@gmail.com>
2023-07-11 10:49:59 +0200

    Added notification parsing for tab, backslash

--- main/src/Notifs/ANCS/Client.cpp
+++ main/src/Notifs/ANCS/Client.cpp
@@ -159,8 +159,10 @@ void ANCS::Client::processData(bool sendIncomplete){
 
 		notif.message = std::regex_replace(notif.message, std::regex("\\\\n"), "\n");
 		notif.message = std::regex_replace(notif.message, std::regex("\\\\r"), "\r");
+		notif.message = std::regex_replace(notif.message, std::regex("\\\\\\"), "\\");
+		notif.message = std::regex_replace(notif.message, std::regex("\\\\t"), "\t");
 		notif.message.erase(std::remove(notif.message.begin(), notif.message.end(), '\r'), notif.message.end());
-
+		std::replace(notif.message.begin(), notif.message.end(), '\t', ' ');
 
 		ESP_LOGI(TAG, "Sending notif 0x%lx. Modify: %d\n", nd.uid, nd.modify);
 		if(nd.modify){
--- main/src/Notifs/Bangle.cpp
+++ main/src/Notifs/Bangle.cpp
@@ -181,7 +181,10 @@ void Bangle::handle_notify(const std::string& line){
 	};
 	notif.message = std::regex_replace(notif.message, std::regex("\\\\n"), "\n");
 	notif.message = std::regex_replace(notif.message, std::regex("\\\\r"), "\r");
+	notif.message = std::regex_replace(notif.message, std::regex("\\\\\\"), "\\");
+	notif.message = std::regex_replace(notif.message, std::regex("\\\\t"), "\t");
 	notif.message.erase(std::remove(notif.message.begin(), notif.message.end(), '\r'), notif.message.end());
+	std::replace(notif.message.begin(), notif.message.end(), '\t', ' ');
 
 	ESP_LOGI(TAG, "New notif ID %ld", notif.uid);
 
