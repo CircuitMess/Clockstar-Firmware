f1088913c0a82fad43324dc03f23136a3b2a3432
Emil Gajšak <emil.gajsak@gmail.com>
2023-07-12 14:36:10 +0200

    Removed Timer std::function usage in ISR, moved Timer methods to IRAM

--- main/src/Periph/Timer.cpp
+++ main/src/Periph/Timer.cpp
@@ -2,14 +2,14 @@
 #include <soc/rtc.h>
 #include <utility>
 
-Timer::Timer(uint32_t period, std::function<void(void*)> ISR, void* dataPtr) : ISR(std::move(ISR)), dataPtr(dataPtr){
+Timer::Timer(uint32_t period, TimerCallback ISR, void* dataPtr) : ISR(ISR), dataPtr(dataPtr){
 
-	ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
+	gptimer_new_timer(&timer_config, &gptimer);
 	gptimer_event_callbacks_t cbs = {
 			.on_alarm = interrupt,
 	};
-	ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, this));
-	ESP_ERROR_CHECK(gptimer_enable(gptimer));
+	gptimer_register_event_callbacks(gptimer, &cbs, this);
+	gptimer_enable(gptimer);
 	setPeriod(period);
 }
 
@@ -20,25 +20,25 @@ Timer::~Timer(){
 }
 
 
-void Timer::start(){
+void IRAM_ATTR Timer::start(){
 	if(state == Running) return;
 
 	state = Running;
-	ESP_ERROR_CHECK(gptimer_start(gptimer));
+	gptimer_start(gptimer);
 }
 
-void Timer::stop(){
+void IRAM_ATTR Timer::stop(){
 	if(state == Stopped) return;
 
 	state = Stopped;
-	ESP_ERROR_CHECK(gptimer_stop(gptimer));
+	gptimer_stop(gptimer);
 }
 
-void Timer::reset(){
-	ESP_ERROR_CHECK(gptimer_set_raw_count(gptimer, 0));
+void IRAM_ATTR Timer::reset(){
+	gptimer_set_raw_count(gptimer, 0);
 }
 
-void Timer::setPeriod(uint32_t period){
+void IRAM_ATTR Timer::setPeriod(uint32_t period){
 	gptimer_alarm_config_t alarm_config = {
 			.alarm_count = period * 1000, // period in us
 			.reload_count = 0,
@@ -49,8 +49,8 @@ void Timer::setPeriod(uint32_t period){
 
 bool IRAM_ATTR Timer::interrupt(gptimer_handle_t timer, const gptimer_alarm_event_data_t* edata, void* user_ctx){
 	auto timerObject = (Timer*) user_ctx;
-	auto cb = timerObject->ISR;
-	if(cb) cb(timerObject->dataPtr);
+	TimerCallback cb = timerObject->ISR;
+	if(cb != nullptr) cb(timerObject->dataPtr);
 
-	return TaskPriority;
+	return false;
 }
