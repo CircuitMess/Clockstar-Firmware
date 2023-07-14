c3a06aeacae630b1d3e5020a94d0034da564b818
Emil Gajšak <emil.gajsak@gmail.com>
2023-07-14 16:56:20 +0200

    Fixed SliderElement range to 0-100

--- main/src/Screens/Settings/SliderElement.h
+++ main/src/Screens/Settings/SliderElement.h
@@ -10,8 +10,8 @@ class SliderElement : public LVSelectable {
 public:
 	explicit SliderElement(lv_obj_t* parent, const char* name, std::function<void(uint8_t)> cb, uint8_t value = 0);
 
-	void setValue(uint8_t value);
-	[[nodiscard]] uint8_t getValue() const;
+	void setValue(uint8_t value); //0-100
+	[[nodiscard]] uint8_t getValue() const; //0-100
 
 private:
 	lv_obj_t* label;
@@ -25,12 +25,13 @@ private:
 	LVStyle sliderMainStyle;
 	LVStyle sliderKnobStyle;
 
-	uint8_t value;
+	uint8_t value; //0-100
 	std::function<void(uint8_t)> cb;
 
-	static constexpr uint8_t SliderWidth = 56;
+	static constexpr uint8_t SliderWidth = 55;
 	static constexpr uint8_t SliderHeight = 12;
-	static constexpr uint8_t SliderRange = 51;
+	static constexpr uint8_t SliderRange = 50;
+	static constexpr uint8_t MaxValue = 100;
 	static constexpr uint8_t Height = 23;
 
 };
