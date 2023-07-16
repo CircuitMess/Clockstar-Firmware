87689aecec42d602508b301f3bcafe752aca9f66
Emil Gajšak <emil.gajsak@gmail.com>
2023-07-16 16:11:06 +0200

    Arpeggiator and ChirpSystem, Timer rework (#24)
    * Added IMU::getSample function, refactored FIFO functions in IMU
    
    * Added Level IMU integration
    
    * Lowered IMU reader task size, added filter reset onStart, refactorings
    
    * Level startup routine fix
    
    * Added Notes definitions to Util
    
    * Removed Timer std::function usage in ISR, moved Timer methods to IRAM
    
    * PWM cleanup, duty setting moved to attach() from setFreq()
    
    * Switched ChirpSystem to drive PWM in a Threaded loop, not ISR
    
    * Removed unnecessary PWM::detach method
    
    * Added ArpeggioSequence abstraction, theremin debug code
    
    * Added PWM checkFrequency to IRAM for use in ISR's
    
    * Reworked ChirpSystem, added ISR-safe play/stop functions
    
    * Theremin Timer ISR for sequence playback
    
    * Added arpeggiator motion controls with tilting
    
    * Removed pause, shortened arpeggiator
    
    * Edited Theremin instruction text, fixed wrong starting tone
    
    * Added theremin semaphore destruction
    
    * Removed unused includes

--- main/src/Periph/Timer.h
+++ main/src/Periph/Timer.h
@@ -2,7 +2,6 @@
 #define CLOCKSTAR_FIRMWARE_TIMER_H
 
 #include <driver/gptimer.h>
-#include <functional>
 #include <esp_attr.h>
 
 /**
@@ -11,13 +10,15 @@
  */
 
 class Timer {
+	typedef void (*TimerCallback)(void* arg);
+
 public:
 	/**
 	 * @param period - Time between ISR calls [ms]
 	 * @param ISR - ISR routine to be called when period time runs out. Must be non-blocking (using fromISR functions)
 	 * @param dataPtr - data pointer passed to ISR routine, optional
 	 */
-	Timer(uint32_t period, std::function<void(void*)> ISR, void* dataPtr = nullptr);
+	Timer(uint32_t period, TimerCallback ISR, void* dataPtr = nullptr);
 	virtual ~Timer();
 
 	void start();
@@ -26,23 +27,22 @@ public:
 	void setPeriod(uint32_t period);
 
 private:
-	static bool IRAM_ATTR interrupt(gptimer_handle_t timer, const gptimer_alarm_event_data_t* edata, void* user_ctx);
+	static bool interrupt(gptimer_handle_t timer, const gptimer_alarm_event_data_t* edata, void* user_ctx);
 
-	static constexpr bool TaskPriority = false;
 	enum {
 		Stopped, Running
 	} state = Stopped;
 
 	gptimer_handle_t gptimer = nullptr;
 
-	gptimer_config_t timer_config = {
+	static constexpr gptimer_config_t timer_config = {
 			.clk_src = GPTIMER_CLK_SRC_DEFAULT,
 			.direction = GPTIMER_COUNT_UP,
 			.resolution_hz = 1000000, // 1MHz, 1 tick=1us
 			.flags = { .intr_shared = 0 }
 	};
 
-	std::function<void(void*)> ISR;
+	TimerCallback ISR = nullptr;
 	void* dataPtr;
 };
 
---
+++ main/src/Screens/Theremin/Theremin.cpp
@@ -0,0 +1,177 @@
+#include <algorithm>
+#include "Theremin.h"
+#include "Util/Services.h"
+#include "Util/stdafx.h"
+
+
+Theremin::Theremin() : audio(*(ChirpSystem*) Services.get(Service::Audio)), sem(xSemaphoreCreateBinary()),
+					   timer(getToneDuration(sequence.getSize()), timerCB, sem),
+					   audioThread([this](){ audioThreadFunc(); }, "Theremin audio", 2048, 5, 1), imu((IMU*) Services.get(Service::IMU)),
+					   pitchFilter(filterStrength), rollFilter(filterStrength){
+	buildUI();
+	sequence.refresh();
+
+	xSemaphoreGive(sem);
+}
+
+
+Theremin::~Theremin(){
+	vSemaphoreDelete(sem);
+}
+
+void Theremin::setOrientation(float pitch, float roll){
+	pitch = std::clamp(pitch, -AngleConstraint, AngleConstraint);
+	roll = std::clamp(roll, -AngleConstraint, AngleConstraint);
+
+	const auto horizontalX = (int16_t) (((roll + AngleConstraint) / (2 * AngleConstraint)) * SliderRange);
+	const auto verticalY = (int16_t) (((pitch + AngleConstraint) / (2 * AngleConstraint)) * SliderRange);
+
+	lv_slider_set_value(sliderVertical, verticalY, LV_ANIM_OFF);
+	lv_slider_set_value(sliderHorizontal, horizontalX, LV_ANIM_OFF);
+
+	const auto mappedNote = (uint16_t) map(roll, -AngleConstraint, AngleConstraint, 0, ArpeggioSequence::MaxBaseNoteIndex);
+	const auto mappedSize = (uint8_t) map(pitch, -AngleConstraint, AngleConstraint, 1, ArpeggioSequence::MaxSequenceSize);
+
+	if(mappedNote != sequence.getBaseNoteIndex()){
+		sequence.setBaseNoteIndex(mappedNote);
+	}
+
+	if(mappedSize != sequence.getSize()){
+		sequence.setSize(mappedSize);
+		timer.setPeriod(getToneDuration(sequence.getSize()));
+	}
+}
+
+void Theremin::onStart(){
+	imu->enableFIFO(false);
+
+	const IMU::Sample reading = imu->getSample();
+	const PitchRoll pitchRoll = { -(float) reading.accelY, -(float) reading.accelX };
+	pitchFilter.reset(pitchRoll.pitch);
+	rollFilter.reset(pitchRoll.roll);
+	setOrientation(pitchRoll.pitch, pitchRoll.roll);
+
+	audioThread.start();
+	timer.start();
+}
+
+void Theremin::onStop(){
+	timer.stop();
+	audio.stop();
+	audioThread.stop();
+}
+
+void Theremin::loop(){
+	const IMU::Sample reading = imu->getSample();
+	const PitchRoll pitchRoll = { (float) pitchFilter.update(-reading.accelY), (float) rollFilter.update(-reading.accelX) };
+	setOrientation(pitchRoll.pitch, pitchRoll.roll);
+}
+
+void IRAM_ATTR Theremin::timerCB(void* arg){
+	BaseType_t priority = pdFALSE;
+	xSemaphoreGiveFromISR(arg, &priority);
+}
+
+void Theremin::audioThreadFunc(){
+	while(!xSemaphoreTake(sem, portMAX_DELAY));
+
+	if(sequenceIndex >= sequence.getSize()){
+		sequenceIndex = 0;
+	}
+
+	if(sequenceIndex == 0){
+		sequence.refresh();
+		timer.setPeriod(getToneDuration(sequence.getSize()));
+	}
+
+	const auto freq = sequence.getTones()[sequenceIndex];
+	const uint16_t toneDuration = getToneDuration(sequence.getSize()) / 2;
+	const Sound sound = { { freq, freq, toneDuration },
+						  { 0,    0,    toneDuration } };
+
+	audio.play(sound);
+
+	sequenceIndex++;
+}
+
+constexpr uint32_t Theremin::getToneDuration(uint8_t sequenceSize){
+	return SequenceDuration / sequenceSize;
+}
+
+void Theremin::buildUI(){
+	lv_style_set_text_color(textStyle, lv_color_white());
+
+
+	bg = lv_obj_create(*this);
+	lv_obj_set_pos(bg, 0, 0);
+	lv_obj_set_size(bg, 128, 128);
+	lv_obj_set_style_bg_img_src(bg, "S:/bg.bin", 0);
+	lv_obj_set_style_bg_img_tiled(bg, true, 0);
+
+
+	sliderHorizontal = lv_slider_create(bg);
+	lv_obj_set_pos(sliderHorizontal, HorizontalBarX, HorizontalBarY);
+	lv_obj_set_size(sliderHorizontal, SliderLength, SliderWidth);
+	lv_obj_set_style_bg_img_src(sliderHorizontal, "S:/theremin/horizontalBar.bin", 0);
+	lv_obj_set_style_bg_img_src(sliderHorizontal, "S:/theremin/dot.bin", LV_PART_KNOB);
+	lv_obj_set_style_pad_hor(sliderHorizontal, 5, LV_PART_MAIN);
+	lv_slider_set_range(sliderHorizontal, 0, SliderRange);
+
+
+	sliderVertical = lv_slider_create(bg);
+	lv_obj_set_pos(sliderVertical, VerticalBarX, VerticalBarY);
+	lv_obj_set_size(sliderVertical, SliderWidth, SliderLength);
+	lv_obj_set_style_bg_img_src(sliderVertical, "S:/theremin/verticalBar.bin", 0);
+	lv_obj_set_style_bg_img_src(sliderVertical, "S:/theremin/dot.bin", LV_PART_KNOB);
+	lv_obj_set_style_pad_bottom(sliderVertical, 5, LV_PART_MAIN);
+	lv_slider_set_range(sliderHorizontal, 0, SliderRange);
+
+
+	textVertical = lv_obj_create(bg);
+	lv_obj_set_layout(textVertical, LV_LAYOUT_FLEX);
+	lv_obj_set_flex_flow(textVertical, LV_FLEX_FLOW_ROW_WRAP);
+	lv_obj_set_flex_align(textVertical, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+	lv_obj_set_size(textVertical, 60, 40);
+	lv_obj_set_pos(textVertical, VerticalTextX, VerticalTextY);
+	lv_obj_set_style_pad_column(textVertical, 3, 0);
+	auto label = lv_label_create(textVertical);
+	lv_label_set_text(label, "Tilt");
+	lv_obj_add_style(label, textStyle, 0);
+	auto arrow = lv_img_create(textVertical);
+	lv_img_set_src(arrow, "S:/theremin/up.bin");
+	lv_obj_set_style_pad_left(arrow, 1, 0);
+	arrow = lv_img_create(textVertical);
+	lv_img_set_src(arrow, "S:/theremin/down.bin");
+	label = lv_label_create(textVertical);
+	lv_label_set_text(label, "to change");
+	lv_obj_add_style(label, textStyle, 0);
+	label = lv_label_create(textVertical);
+	lv_label_set_text(label, "the number");
+	lv_obj_add_style(label, textStyle, 0);
+	label = lv_label_create(textVertical);
+	lv_label_set_text(label, "of tones");
+	lv_obj_add_style(label, textStyle, 0);
+
+
+	textHorizontal = lv_obj_create(bg);
+	lv_obj_set_layout(textHorizontal, LV_LAYOUT_FLEX);
+	lv_obj_set_flex_flow(textHorizontal, LV_FLEX_FLOW_ROW_WRAP);
+	lv_obj_set_flex_align(textHorizontal, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+	lv_obj_set_size(textHorizontal, 80, 40);
+	lv_obj_set_pos(textHorizontal, HorizontalTextX, HorizontalTextY);
+	lv_obj_set_style_pad_column(textHorizontal, 3, 0);
+	label = lv_label_create(textHorizontal);
+	lv_label_set_text(label, "Tilt");
+	lv_obj_add_style(label, textStyle, 0);
+	arrow = lv_img_create(textHorizontal);
+	lv_img_set_src(arrow, "S:/theremin/left.bin");
+	lv_obj_set_style_pad_left(arrow, 1, 0);
+	arrow = lv_img_create(textHorizontal);
+	lv_img_set_src(arrow, "S:/theremin/right.bin");
+	label = lv_label_create(textHorizontal);
+	lv_label_set_text(label, "to change");
+	lv_obj_add_style(label, textStyle, 0);
+	label = lv_label_create(textHorizontal);
+	lv_label_set_text(label, "base frequency");
+	lv_obj_add_style(label, textStyle, 0);
+}
