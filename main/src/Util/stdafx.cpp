f0ab7cff0e3c2a4d81dbb095e6534808b452bbc1
Filip Budiša <filip@circuitmess.com>
2023-07-20 12:50:09 +0200

    Add stackRep and heapRep to stdafx

--- main/src/Util/stdafx.cpp
+++ main/src/Util/stdafx.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include <esp_timer.h>
 #include <esp_heap_caps.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 uint64_t millis(){
 	return micros() / 1000;
@@ -13,4 +15,23 @@ uint64_t micros(){
 void ramReport(){
 	printf("Free INTERNAL heap: %zu B, largest block %zu B\n", heap_caps_get_free_size(MALLOC_CAP_INTERNAL), heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL));
 	// printf("Free PSRAM heap: %zu B, largest block %zu B\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM), heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM));
+}
+
+void stackRep(const char* where){
+	if(where){
+		printf("%s: ", where);
+	}
+
+	auto free = uxTaskGetStackHighWaterMark(nullptr);
+	printf("min. free stack: %u B\n", free);
+}
+
+void heapRep(const char* where){
+	if(where){
+		printf("%s:\n", where);
+	}
+
+	printf("Free 32b heap: %zu B, largest block %zu B\n", heap_caps_get_free_size(MALLOC_CAP_32BIT), heap_caps_get_largest_free_block(MALLOC_CAP_32BIT));
+	printf("Free 8b  heap: %zu B, largest block %zu B\n", heap_caps_get_free_size(MALLOC_CAP_8BIT), heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
+	printf("\n");
 }
