ec25ecb79852f6d4811194f5a4ebe91ebfb1fa33
Filip Budiša <filip@circuitmess.com>
2023-07-25 21:32:50 +0200

    Adjust Battery level hysteresis params

--- main/src/Devices/Battery.cpp
+++ main/src/Devices/Battery.cpp
@@ -10,7 +10,7 @@
 #define MIN_READ 3050 // 3.6V
 
 Battery::Battery() : Threaded("Battery", 2048, 4), adc((gpio_num_t) PIN_BATT, 0.05, MIN_READ, MAX_READ, getVoltOffset()),
-					 hysteresis({ 0, 1, 15, 30, 60, 100 }, 5),
+					 hysteresis({ 0, 4, 15, 30, 70, 100 }, 3),
 					 chargeHyst(2000, false), sem(xSemaphoreCreateBinary()), timer(ShortMeasureIntverval, isr, sem){
 
 	gpio_config_t cfg_gpio = {};
