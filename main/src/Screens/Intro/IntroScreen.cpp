e4867c9acc13fa462e392bdec0c8f3a7982a7e0c
Filip Budiša <filip@circuitmess.com>
2024-02-25 21:02:44 +0100

    Add Artemis logo fade to IntroScreen

--- main/src/Screens/Intro/IntroScreen.cpp
+++ main/src/Screens/Intro/IntroScreen.cpp
@@ -1,6 +1,7 @@
 #include "IntroScreen.h"
 #include "Util/stdafx.h"
 #include "Screens/Lock/LockScreen.h"
+#include "LV_Interface/FSLVGL.h"
 
 IntroScreen::IntroScreen(){
 	bg = lv_obj_create(*this);
@@ -19,39 +20,75 @@ IntroScreen::IntroScreen(){
 		lv_img_set_src(images[i], ImagePaths[i]);
 		lv_obj_center(images[i]);
 	}
+
+	for(const auto img : Cached){
+		FSLVGL::addToCache(img);
+	}
 }
 
-void IntroScreen::loop(){
-	if(state == Scrolling && millis() - counter >= ScrollTime){
-		state = Waiting;
-		counter = millis();
-	}else if(state == Waiting && millis() - counter >= WaitTime){
-		currentIndex++;
-		if(currentIndex >= 2){
-			transition([](){ return std::make_unique<LockScreen>(); });
-			return;
-		}
-		state = Scrolling;
-		counter = millis();
-		const auto currentScroll =  lv_obj_get_scroll_y(*this);
-		lv_anim_set_values(&anim, currentScroll, currentScroll +  128);
-		lv_anim_start(&anim);
+IntroScreen::~IntroScreen(){
+	for(const auto img : Cached){
+		FSLVGL::removeFromCache(img);
 	}
 }
 
 void IntroScreen::onStart(){
-	counter = millis();
+	animTime = millis();
 	lv_anim_init(&anim);
-	lv_anim_set_exec_cb(&anim, animFunc);
+	lv_anim_set_exec_cb(&anim, animFuncScroll);
 	lv_anim_set_var(&anim, *this);
 	lv_anim_set_time(&anim, ScrollTime);
 	lv_anim_set_path_cb(&anim, lv_anim_path_ease_in_out);
 }
 
-void IntroScreen::animFunc(void* obj, int32_t val){
+void IntroScreen::onStop(){
+	lv_anim_del(*this, nullptr);
+}
+
+void IntroScreen::loop(){
+	if(state == Scrolling && millis() - animTime >= ScrollTime){
+		state = Waiting;
+		animTime = millis();
+	}else if(state == Waiting && millis() - animTime >= WaitTime){
+		currentIndex++;
+		if(currentIndex >= 3){
+			transition([](){ return std::make_unique<LockScreen>(); });
+			return;
+		}else if(currentIndex >= 2){
+			gotoPhase2();
+			lv_anim_set_var(&anim, phase2);
+			lv_anim_set_values(&anim, 0, LV_OPA_COVER);
+			lv_anim_set_exec_cb(&anim, animFuncOpa);
+		}else{
+			const auto currentScroll =  lv_obj_get_scroll_y(*this);
+			lv_anim_set_values(&anim, currentScroll, currentScroll +  128);
+		}
+		state = Scrolling;
+		animTime = millis();
+		lv_anim_start(&anim);
+	}
+}
+
+void IntroScreen::gotoPhase2(){
+	phase2 = lv_obj_create(*this);
+	lv_obj_add_flag(phase2, LV_OBJ_FLAG_FLOATING);
+	lv_obj_set_size(phase2, 128, 128);
+	lv_obj_set_pos(phase2, 0, 0);
+
+	lv_obj_set_style_bg_img_src(phase2, "S:/intro/blackBg.bin", 0);
+	lv_obj_set_style_bg_img_opa(phase2, LV_OPA_COVER, 0);
+
+	auto img = lv_img_create(phase2);
+	lv_img_set_src(img, "S:/intro/artemis.bin");
+	lv_obj_center(img);
+
+	lv_obj_set_style_opa(phase2, 0, 0);
+}
+
+void IntroScreen::animFuncScroll(void* obj, int32_t val){
 	lv_obj_scroll_to_y((lv_obj_t*)obj, val, LV_ANIM_OFF);
 }
 
-void IntroScreen::onStop(){
-	lv_anim_del(*this, nullptr);
+void IntroScreen::animFuncOpa(void* obj, int32_t val){
+	lv_obj_set_style_opa((lv_obj_t*) obj, val, 0);
 }
