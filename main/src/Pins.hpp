882ab284ce643e8a414e06d69f05c02b0eaf88db
Emil Gajšak <emil.gajsak@gmail.com>
2024-03-04 13:28:28 +0100

    Reverted Pins.hpp

--- main/src/Pins.hpp
+++ main/src/Pins.hpp
@@ -1,44 +1,44 @@
 #ifndef CLOCKSTAR_LIBRARY_PINS_HPP
 #define CLOCKSTAR_LIBRARY_PINS_HPP
 
-#define BTN_DOWN 39
-#define BTN_UP 37
-#define BTN_SELECT 38
-#define BTN_ALT 1
+#define BTN_DOWN 10
+#define BTN_UP 9
+#define BTN_SELECT 8
+#define BTN_ALT 21
 
 #define BTN_LEFT BTN_UP
 #define BTN_RIGHT BTN_DOWN
 #define BTN_A BTN_SELECT
 #define BTN_B BTN_ALT
 
-#define RGB_R 14
-#define RGB_G 13
-#define RGB_B 12
+#define RGB_R 33
+#define RGB_G 34
+#define RGB_B 48
 
-#define PIN_BL 7
-#define PIN_BUZZ 47
-#define PIN_BATT 2
-#define PIN_CHARGE 21
-#define PIN_PWDN 26
+#define PIN_BL 37
+#define PIN_BUZZ 11
+#define PIN_BATT 6
+#define PIN_CHARGE 47
+#define PIN_PWDN 42
 
-#define I2C_SDA 3
-#define I2C_SCL 4
+#define I2C_SDA 35
+#define I2C_SCL 36
 
-#define TFT_SCK 11
-#define TFT_MOSI 10
-#define TFT_DC 9
-#define TFT_RST 8
+#define TFT_SCK 41
+#define TFT_MOSI 40
+#define TFT_DC 39
+#define TFT_RST 38
 
 #define JIG_STATUS 0
 
-#define IMU_INT1 17
-#define IMU_INT2 18
+#define IMU_INT1 4
+#define IMU_INT2 5
 
-#define LED_1 5
-#define LED_2 6
-#define LED_3 40
-#define LED_4 41
-#define LED_5 42
-#define LED_6 43
+#define LED_1 46
+#define LED_2 45
+#define LED_3 44
+#define LED_4 43
+#define LED_5 18
+#define LED_6 17
 
 #endif //CLOCKSTAR_LIBRARY_PINS_HPP
