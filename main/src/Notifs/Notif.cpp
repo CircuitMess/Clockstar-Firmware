2890445a1d9eed6b5a40e41c989a868f8a9b5c54
Fr0zen3k <jan.kresic@circuitmess.com>
2024-03-13 12:04:01 +0100

    Icon path getting now returns theme dependent path, lvgl thread stack size adjustment

--- main/src/Notifs/Notif.cpp
+++ main/src/Notifs/Notif.cpp
@@ -1,33 +1,42 @@
 #include "Notif.h"
 #include <unordered_map>
 #include "Filepaths.hpp"
-
-static const std::unordered_map<std::string, const char*> appMap = {
-		{ "Messenger", File::Icons::Theme1::AppMess },
-		{ "WhatsApp",  File::Icons::Theme1::AppWapp },
-		{ "Messages",  File::Icons::Theme1::AppSms },
-		{ "Instagram", File::Icons::Theme1::AppInst },
-		{ "Snapchat",  File::Icons::Theme1::AppSnap },
-		{ "TikTok",    File::Icons::Theme1::AppTiktok }
-};
-
-static const std::unordered_map<Notif::Category, const char*> catMap = {
-		{ Notif::Category::Other,              File::Icons::Theme1::CatOther },
-		{ Notif::Category::IncomingCall,       File::Icons::Theme1::CallIn },
-		{ Notif::Category::MissedCall,         File::Icons::Theme1::CallMiss },
-		{ Notif::Category::Voicemail,          File::Icons::Theme1::CatOther },
-		{ Notif::Category::Social,             File::Icons::Theme1::CatSoc },
-		{ Notif::Category::Schedule,           File::Icons::Theme1::CatSched },
-		{ Notif::Category::Email,              File::Icons::Theme1::CatEmail },
-		{ Notif::Category::News,               File::Icons::Theme1::CatNews },
-		{ Notif::Category::HealthAndFitness,   File::Icons::Theme1::CatHealth },
-		{ Notif::Category::BusinessAndFinance, File::Icons::Theme1::CatFin },
-		{ Notif::Category::Location,           File::Icons::Theme1::CatLoc },
-		{ Notif::Category::Entertainment,      File::Icons::Theme1::CatEntert },
-		{ Notif::Category::OutgoingCall,       File::Icons::Theme1::CallOut }
-};
+#include "Settings/Settings.h"
+#include "Util/Services.h"
 
 const char* iconPath(const Notif& notif){
+	Settings* settings = (Settings*) Services.get(Service::Settings);
+	if(settings == nullptr){
+		return "";
+	}
+
+	const Theme theme = settings->get().themeData.theme;
+
+	const std::unordered_map<std::string, const char*> appMap = {
+			{ "Messenger", THEMED_FILE(Icons, AppMess, theme) },
+			{ "WhatsApp",  THEMED_FILE(Icons, AppWapp, theme) },
+			{ "Messages",  THEMED_FILE(Icons, AppSms, theme) },
+			{ "Instagram", THEMED_FILE(Icons, AppInst, theme) },
+			{ "Snapchat",  THEMED_FILE(Icons, AppSnap, theme) },
+			{ "TikTok",    THEMED_FILE(Icons, AppTiktok, theme) }
+	};
+
+	const std::unordered_map<Notif::Category, const char*> catMap = {
+			{ Notif::Category::Other,              THEMED_FILE(Icons, CatOther, theme) },
+			{ Notif::Category::IncomingCall,       THEMED_FILE(Icons, CallIn, theme) },
+			{ Notif::Category::MissedCall,         THEMED_FILE(Icons, CallMiss, theme) },
+			{ Notif::Category::Voicemail,          THEMED_FILE(Icons, CatOther, theme) },
+			{ Notif::Category::Social,             THEMED_FILE(Icons, CatSoc, theme) },
+			{ Notif::Category::Schedule,           THEMED_FILE(Icons, CatSched, theme) },
+			{ Notif::Category::Email,              THEMED_FILE(Icons, CatEmail, theme) },
+			{ Notif::Category::News,               THEMED_FILE(Icons, CatNews, theme) },
+			{ Notif::Category::HealthAndFitness,   THEMED_FILE(Icons, CatHealth, theme) },
+			{ Notif::Category::BusinessAndFinance, THEMED_FILE(Icons, CatFin, theme) },
+			{ Notif::Category::Location,           THEMED_FILE(Icons, CatLoc, theme) },
+			{ Notif::Category::Entertainment,      THEMED_FILE(Icons, CatEntert, theme) },
+			{ Notif::Category::OutgoingCall,       THEMED_FILE(Icons, CallOut, theme) }
+	};
+
 	auto itApp = appMap.find(notif.appID);
 	if(itApp != appMap.end()){
 		return itApp->second;
@@ -38,5 +47,5 @@ const char* iconPath(const Notif& notif){
 		return itCat->second;
 	}
 
-	return File::Icons::Theme1::CatOther;
+	return THEMED_FILE(Icons, CatOther, theme);
 }
