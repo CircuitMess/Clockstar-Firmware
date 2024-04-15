b3459ba89d686512517f007e004a886c639284f4
Fr0zen3k <jan.kresic@circuitmess.com>
2024-04-15 16:36:14 +0200

    Removed leftover redundancy

--- main/src/Screens/Settings/LoadingModal.cpp
+++
@@ -1,8 +0,0 @@
-#include "LoadingModal.h"
-#include "Settings/Settings.h"
-
-LoadingModal::LoadingModal(LVScreen *parent) : LVModal(parent) {
-	text = new LabelElement(*this, "Loading...", [](){}, false, LV_ALIGN_CENTER);
-
-	lv_obj_set_align(*text, LV_ALIGN_CENTER);
-}
